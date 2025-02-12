6eb8cfd3a4ce808eea00fffe97aea5ade4d8928b
DriverStation1714 <164568014+DriverStation1714@users.noreply.github.com>
2025-02-11 19:00:48 -0600

    Subsystem made

---
+++ src/main/java/frc/robot/subsystems/Funnel.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Funnel extends SubsystemBase {
+  private int m_FunnelID = 0;
+  private int currentLimit = 0;
+  private double funnelGearRatio = 0.0;
+
+
+  private SparkMax m_Funnel;
+  private SparkMaxConfig funnelConfig;
+  /** Creates a new Funnel. */
+  public Funnel() {
+    m_Funnel =  new SparkMax(m_FunnelID, MotorType.kBrushless);
+    funnelConfig = new SparkMaxConfig();
+
+    funnelConfig.encoder
+      .positionConversionFactor(funnelGearRatio)
+      .velocityConversionFactor(funnelGearRatio);
+    funnelConfig
+    .inverted(false)
+    .idleMode(IdleMode.kCoast)
+    .smartCurrentLimit(currentLimit);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
