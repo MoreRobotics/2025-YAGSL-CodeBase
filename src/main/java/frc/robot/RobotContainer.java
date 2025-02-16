731533855ad436a217a617b8ad107ea55e9df983
Driver Station <driverstation1714@gmail.com>
2025-02-16 14:05:08 -0600

    tuned elevator
    Co-Authored-By: Samuel Moore <77031467+Samuel-Moore-1714@users.noreply.github.com>
    Co-Authored-By: Karl Akert <1631339+kpakert@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -203,8 +203,8 @@ public class RobotContainer {
         //driverDpadLeft.onTrue(s_Swerve.pathfindiCommand);
         driverA.onTrue(new InstantCommand(() -> s_Elevator.setElevatorPosition(0)));
         driverB.onTrue(new InstantCommand(() -> s_Elevator.setElevatorPosition(0)));
-        driverX.onTrue(new InstantCommand(() -> s_Elevator.setElevatorPosition(82.77)));
-        driverY.onTrue(new InstantCommand(() -> s_Elevator.setElevatorPosition(182.239)));
+        driverX.onTrue(new InstantCommand(() -> s_Elevator.setElevatorPosition(84.77)));
+        driverY.onTrue(new InstantCommand(() -> s_Elevator.setElevatorPosition(184.239)));
 
     }
     /**
